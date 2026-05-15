8f1b082a670dc657b0036f253c57eacc8375d493
unknown <paulo.stduarte@gmail.com>
2026-05-15 17:03:58 -0300

    ta pronto

--- app/(tabs)/edtImage.tsx
+++ app/(tabs)/edtImage.tsx
@@ -1,28 +1,52 @@
-import {View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-
+import { View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('@/assets/images/pizza.webp');
 
-export default function EdtImage() {
+export default function Index() {
+ const pickImageAsync = async () => {
+  let result = await ImagePicker.launchImageLibraryAsync({
+     mediaTypes: ['images'],
+     allowsEditing: true,
+     quality: 1,
+  });
+ 
+  if (!result.canceled) {
+    console.log(result);
+  } else {
+     alert('You did not select any image.');
+  }
+};
+
+ 
   return (
-   <View style={styles.container}>
-     <View style={styles.imageContainer}>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
-     </View>
-   </View>
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="escolha uma foto" />
+        <Button label="use a foto" />
+      </View>
+    </View>
   );
 }
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
   },
+
   imageContainer: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: 28,
+  },
 
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
